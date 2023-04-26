055ab50c32e9a04128bde64e5325b46ebf896e33
Hubert Plisiecki <hplisiecki@gmail.com>
2023-04-26 16:52:16 +0200

    update

--- doc2vec/get_vectors.py
+++ doc2vec/get_vectors.py
@@ -6,12 +6,16 @@ model = Doc2Vec.load(save_path + 'doc2vec_0.model')
 
 corpus = pd.read_csv(r'D:\PycharmProjects\parliamentary_emotions\data/abortion_with_metrics') #### INNY KORPUS
 
+
 person_labels = ['PERSON_TAG' + '_' + str(row[1]['kto']) for row in corpus.iterrows()]
 person_dict = {tag: tag.replace('PERSON_TAG_', '') for tag in person_labels}
 person_reverse_dict = {tag.replace('PERSON_TAG_', ''): tag for tag in person_labels}
 
-# ZBIERAMY WEKTORY
-person_vectors = {}
-for person in corpus['kto'].unique():
-    person_vectors[person] = model.dv[person_reverse_dict[person]]
+for term in corpus['term'].unique():
+    temp_corpus = corpus[corpus['term'] == term]
+
+    # ZBIERAMY WEKTORY
+    person_vectors = {}
+    for person in temp_corpus['kto'].unique():
+        person_vectors[person + '_' + term] = model.dv[person_reverse_dict[person]]
 
